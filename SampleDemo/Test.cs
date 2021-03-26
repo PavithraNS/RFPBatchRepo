fba710367741ba50a51e758349d5a7a85c15b095
Pavithra <nspavi1991@gmail.com>
2021-03-26 10:02:54 +0530

    Add sample project

---
+++ SampleDemo/Test.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using TestClass;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleDemo
+{
+    /// <summary>
+    /// Tested this sample application for printing hello message
+    /// </summary>
+    class Test
+    {
+        /// <summary>
+        /// Main method is the start point for console application
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Welcome to fellowship");
+            Test program = new Test();
+            TestClass.Test p = new TestClass.Test();
+            p.Test1();
+            double res = program.TestMethod();
+            Console.WriteLine("Double value in main method " + res);
+            
+            Console.Read();
+        }
+
+        public double TestMethod()
+        {
+            double result = 2.3 + 4.5;
+            Console.WriteLine("Double value in testMethod " + result);
+            return result;
+        }
+
+    }
+}
